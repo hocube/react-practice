ad632a35030cedfe93ac78c6e4676d42e323a1fa
hocube <hocube159@gmail.com>
2023-12-23 15:59:02 +0900

    동적인UI만들기, map 개념 및 실습

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ function App() {
 
   let post = '다산 우동 맛집';
   let [글제목, 글제목변경] = useState(['강아지 이름 추천', '홍대 우동 맛집', '파이썬 독학']);
-  let [좋아요, 좋아요변경] = useState(0);
+  let [좋아요, 좋아요변경] = useState([0, 0, 0]);
+  let [modal, setModal] = useState(false); 
+
+  [1,2,3].map(function(a){
+    return '123123123'
+  })
 
   return (
     <div className="App">
@@ -28,7 +33,7 @@ function App() {
         글제목변경(copy1);
       }}>가나다정렬</button>
 
-      <div className='list'>
+      {/* <div className='list'>
         <h4>{ 글제목[0] } <span onClick={ ()=>{ 좋아요변경(좋아요+1) }}>👍</span> {좋아요} </h4>
         <p>12월 21일 발행</p>
       </div>
@@ -37,12 +42,30 @@ function App() {
         <p>12월 21일 발행</p>
       </div>
       <div className='list'>
-        <h4>{ 글제목[2] }</h4>
+        <h4 onClick={()=>{ setModal(!modal) }}>{ 글제목[2] }</h4>
         <p>12월 21일 발행</p>
-      </div>
+      </div> */}
 
-      <Modal></Modal>
-      
+      {
+        modal ==  true ? <Modal/> : null
+                          //참     //거짓
+      }
+
+      {
+        글제목.map(function(a, i){
+          return (
+          <div className='list'>
+            <h4>{ 글제목[i] } 
+              <span onClick={()=>{
+                let copy좋아요 = [...좋아요];
+                copy좋아요[i] = copy좋아요[i]+1;
+                좋아요변경(copy좋아요)}}>👍</span> {좋아요[i]} 
+            </h4>
+            <p>12월 21일 발행</p>
+          </div>
+          )
+        })
+      }
     </div>
   );
 }
