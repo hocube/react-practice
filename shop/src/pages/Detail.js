3b828937f9704d628fce212e44367de417650a99
hocube <hocube159@gmail.com>
2024-01-10 14:49:43 +0900

    Lifecycle과 useEffect

--- shop/src/pages/Detail.js
+++ shop/src/pages/Detail.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from 'styled-components';
 
@@ -15,14 +16,35 @@ let YellowBtn = styled.button`
 function Detail(props) {
 
   let{id} = useParams();
+  let [count, setCount] = useState(0)
+  let [alert, setAlert] = useState(true)     // true로 바꾸면 노란박스 보이게, false로 바꾸면 안보이게.
+  let [num, setNum] = useState('')
+
+  useEffect(()=>{
+    let timer = setTimeout(()=>{setAlert(false)}, 2000)
+    return()=>{
+      clearTimeout(timer);
+    }
+  }, [])
+
+  useEffect(()=>{
+    if(isNaN(num) == true){
+      alert('이시키야 한글입력해라')
+    }
+  }, [num])
 
   return (
     <div className="container">
+      {
+         alert == true
+         ? <div className="alert alert-warning"> 2초 이내 구매시 할인 </div> : null
+      }
       <div className="row">
         <div className="col-md-6">
           <img src={props.product[id].img} width="100%"/>
         </div>
         <div className="col-md-6 mt-4">
+          <input onChange={ (e)=>{setNum(e.target.value)} } />
           <h4 className="pt-5">{props.product[id].title}</h4>
           <p>{props.product[id].content}</p>
           <p>{props.product[id].price}원</p>
